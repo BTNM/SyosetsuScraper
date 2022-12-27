2a7f46b452677f6e01741d466785d9b17b994731
BaoTNM <BaoTNguyenM@gmail.com>
2022-12-27 15:42:54 +0100

    add class NovelItem

--- scraper/scraper/items.py
+++ scraper/scraper/items.py
@@ -6,7 +6,15 @@
 import scrapy
 
 
-class ScraperItem(scrapy.Item):
+class NovelItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    novel_title = scrapy.Field()
+    novel_description = scrapy.Field()
+    volume_title = scrapy.Field()
+    chapter_start_end = scrapy.Field()
+    chapter_number = scrapy.Field()
+    chapter_title = scrapy.Field()
+    chapter_foreword = scrapy.Field()
+    chapter_text = scrapy.Field()
+    chapter_afterword = scrapy.Field()
