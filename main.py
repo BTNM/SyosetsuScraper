2b739a7acb6924537c04af109789187d70988817
BaoTNM <BaoTNguyenM@gmail.com>
2023-10-15 21:08:37 +0200

    remove unused comments and moved func and layout

--- main.py
+++ main.py
@@ -4,60 +4,8 @@ import PySimpleGUI as sg
 import src.GUI.layout as layout
 import src.GUI.table_operations as tabop
 import multiprocessing
-import csv
-import os
 import threading
 import re
-
-
-# def load_table(filepath):
-#     """
-#     Read a CSV file at the given file path and return its data as a list of lists, excluding the header.
-#     Args:
-#         filepath (str): The path of the CSV file to be read.
-#     Returns:
-#         list: The list of lists containing the CSV data, excluding the header.
-#     """
-#     data = []
-#     if not os.path.exists(filepath):
-#         # Create the file if it doesn't exist
-#         with open(filepath, "w", newline="") as file:
-#             writer = csv.writer(file)
-#             # Write the header row if needed
-#             writer.writerow(["Name", "URL", "Range"])
-#     else:
-#         try:
-#             with open(filepath, "r", encoding="utf-8") as file:
-#                 csv_reader = csv.reader(file)
-#                 headers = next(csv_reader)  # Skip the header
-#                 for row in csv_reader:
-#                     data.append(row)
-#         except FileNotFoundError:
-#             print("File not found: {}".format(filepath))
-#             return []
-#     return data
-
-
-# def export_table_csv(table: list, tablename):
-#     """
-#     Save the content of the table to a CSV file in the specified directory path.
-#     Args:
-#         table (list): The list of lists representing the table data.
-#         tablename (str): The name of the CSV file.
-#     """
-#     file_path = "D:\VisualStudioProjects\SyosetsuScraper\src\storage\{}.csv".format(
-#         tablename
-#     )
-#     header = [["Name", "URL", "Range"]]
-
-#     try:
-#         with open(file_path, mode="w", newline="", encoding="utf-8") as csv_file:
-#             # csv_writer = csv.writer(csv_file, quotechar='"', quoting=csv.QUOTE_ALL)
-#             csv_writer = csv.writer(csv_file)
-#             csv_writer.writerows(header)
-#             csv_writer.writerows(table)
-#     except IOError:
-#         print("Error writing to CSV file: {}".format(file_path))
 
 
 # load data from storage file for persistent data
@@ -97,81 +45,6 @@ def run_multiprocess_crawl(novel_list, log_queue, window, start_chapter=None):
     )
     sfs.text_output_files(novel_list, start_chapter)
     print("web scraping all novels in table finished\n")
-
-
-# def handle_delete_button_event(values):
-#     if values["history_table"]:
-#         del history_table_data[values["history_table"][0]]
-#         window["input_table"].update(values=history_table_data)
-#         window["history_table"].update(values=history_table_data)
-#     elif values["scraped_table"]:
-#         del scraped_table_data[values["scraped_table"][0]]
-#         window["scraped_table"].update(values=scraped_table_data)
-
-
-# def handle_deselect_button_event(values):
-#     if values["history_table"]:
-#         window["history_table"].update(select_rows=[])
-#     if values["scraped_table"]:
-#         window["scraped_table"].update(select_rows=[])
-
-
-# def transfer_rows(window, source_table_key, destination_table_key):
-#     selected_rows = window[source_table_key].SelectedRows
-#     if source_table_key == "history_table":
-#         source_table_data = history_table_data
-#         destination_table_data = scraped_table_data
-#     elif source_table_key == "scraped_table":
-#         source_table_data = scraped_table_data
-#         destination_table_data = history_table_data
-
-#     if selected_rows:
-#         selected_row = [source_table_data[index] for index in selected_rows]
-#         selected_data = selected_row[0]
-#         if selected_data not in destination_table_data:
-#             destination_table_data.append(selected_data)
-#             window[destination_table_key].update(values=destination_table_data)
-#             # update input_table on front page if transfer to history_table
-#             if source_table_key == "scraped_table":
-#                 window["input_table"].update(values=destination_table_data)
-
-
-# def move_rows_up(window_table):
-#     selected_rows = window_table.SelectedRows  # Get the selected row indexes
-#     if selected_rows:
-#         table_data = window_table.Get()
-#         for row in selected_rows:
-#             if row > 0:
-#                 # Swap the selected row with the row above it
-#                 table_data[row], table_data[row - 1] = (
-#                     table_data[row - 1],
-#                     table_data[row],
-#                 )
-#         # Update the table with the modified data
-#         window_table.Update(values=table_data)
-
-
-# def move_rows_down(window_table):
-#     selected_rows = window_table.SelectedRows  # Get the selected row indexes
-#     if selected_rows:
-#         table_data = window_table.Get()
-#         for row in reversed(selected_rows):
-#             if row < len(table_data) - 1:
-#                 # Swap the selected row with the row below it
-#                 table_data[row], table_data[row + 1] = (
-#                     table_data[row + 1],
-#                     table_data[row],
-#                 )
-
-#         # Update the table with the modified data
-#         window_table.Update(values=table_data)
-
-
-# def export_table_data(table_key):
-#     table_values = window[table_key].get()
-#     novel_list = [row for row in table_values]
-#     window["tab2_output_text"].update(f"table_data:{novel_list}")
-#     tabop.export_table_csv(novel_list, table_key)
 
 
 def update_progress_bar_print_logs(window, log_queue):
@@ -219,17 +92,6 @@ def update_progress_bar_print_logs(window, log_queue):
     window["output_terminal"].print(log_message, end="")
 
 
-# # Custom input validation function
-# def is_input_valid_integer(value, field_name):
-#     try:
-#         # return int(value)
-#         if int(value):
-#             return True
-#     except ValueError:
-#         print(f"Wrong Input for {field_name}: {value} \nPlease enter a valid integer.")
-#         return False
-
-
 # Create a multiprocessing queue to store the log messages
 log_queue = multiprocessing.Queue()
 # Initialize the data list and table data from storage
@@ -237,10 +99,6 @@ history_table_data = history_table_load_data
 scraped_table_data = scraped_table_load_data
 # TODO: make executable, desktop app or
 # TODO: create docker image and run with a docker container
-# TODO: add variable for chapter number to start crawl from
-# TODO: refactor code, split out layout and methods etc
-# TODO: if given starting chapter then use it on selected novel crawl
-
 # TODO: add a fourth column to table_data to include latest chapter at that time when scrawled or null if not crawled yet
 # TODO: add check for link to check current latest chapter number for novel, maybe scrape html content table size or last element, in the novel history tab
 
@@ -280,7 +138,6 @@ if __name__ == "__main__":
                 )
                 window["range"].update("")
                 continue
-
             history_table_data.append([name, url, range_val])
             window["input_table"].update(values=history_table_data)
             window["history_table"].update(values=history_table_data)
@@ -299,10 +156,7 @@ if __name__ == "__main__":
                 selected_novel_list.append(tuple_data)
                 window["output_terminal"].print(f"Selected rows: {tuple_data}")
                 # Create multiprocessing processes for each novel web scraping in the background
-
-                # window["progress_text"].update(f"Progress: {selected_data[0][0]}:")
                 # run_multiprocess_crawl(novel_list, log_queue)
-
                 start_chapter = values["input_starting_chapter"]
                 # Run the crawling process in a separate thread
                 crawling_thread = threading.Thread(
@@ -311,7 +165,6 @@ if __name__ == "__main__":
                 )
                 crawling_thread.start()
 
-                # sfs.text_output_files(novel_list)
                 if selected_data[0] not in scraped_table_data:
                     scraped_table_data.append(selected_data[0])
                     window["scraped_table"].update(values=scraped_table_data)
@@ -321,7 +174,7 @@ if __name__ == "__main__":
             novel_list = [tuple(row) for row in table_values]
             window["output_terminal"].print(f"table_data:{novel_list}")
             # run_multiprocess_crawl(novel_list, log_queue)
-            # sfs.text_output_files(novel_list)
+
             # Run the crawling process in a separate thread
             crawling_thread = threading.Thread(
                 target=run_multiprocess_crawl, args=(novel_list, log_queue, window)
@@ -332,7 +185,6 @@ if __name__ == "__main__":
                 if row not in scraped_table_data:
                     scraped_table_data.append(row)
             window["scraped_table"].update(values=scraped_table_data)
-            # window["output_terminal"].print(f"Web Scrapeing novel: {novelname} finished\n")
         # Handle events from the "Novel History" tab
         if event == "table_row_delete_btn":
             tabop.handle_delete_button_event(
