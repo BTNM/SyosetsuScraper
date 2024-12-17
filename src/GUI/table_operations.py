57ce3061640127bb40ba35f5b193eb0a30c02228
BTNM <BaoTNguyenM@gmail.com>
2024-12-17 23:55:45 +0100

    refactor: get_novel_latest_chapter func for ncode and nocturne novels

--- src/GUI/table_operations.py
+++ src/GUI/table_operations.py
@@ -1,23 +1,19 @@
-from bs4 import BeautifulSoup
-import requests
 import csv
 import os
 import logging
+import requests
+from processing.processing_constants import PROJECT_ROOT_PATH
+from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from processing.processing_constants import STORAGE_PATH, PROJECT_ROOT_PATH
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-# import time
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from selenium.webdriver.chrome.options import Options
-# from webdriver_manager.chrome import ChromeDriverManager
+# Reduce the verbosity of the logs
+logging.basicConfig(level=logging.INFO)
 
-# # Reduce the verbosity of the logs
-# logging.basicConfig(level=logging.INFO)
-# logging.getLogger("urllib3").setLevel(logging.WARNING)
-# logging.getLogger("selenium").setLevel(logging.WARNING)
-# logging.getLogger("WDM").setLevel(logging.WARNING)
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110"
+}
 
 
 def load_table(folder_path, tablename):
@@ -208,147 +204,110 @@ def is_input_valid_integer(value, field_name):
         return False
 
 
-def get_novel_latest_chapter(url: str):
+def get_novel_latest_chapter(url: str) -> int | None:
     latest_chapter = None
     if "ncode.syosetu.com" in url:
         return get_novel_latest_chapter_ncode(url)
-    # elif "novel18.syosetu.com" in url:
-    #     return get_novel_latest_chapter_nocturne(url)
+    elif "novel18.syosetu.com" in url:
+        return get_novel_latest_chapter_nocturne(url)
     else:
         print(f"Get Novel Lastest Chapter didn't run with url={url}")
         return latest_chapter
 
 
-# def get_novel_latest_chapter_nocturne(url: str) -> int:
-#     # Set up Selenium WebDriver with headless option and logging preferences
-#     options = Options()
-#     options.add_argument("--headless")  # Run in headless mode
-#     options.add_argument("--disable-gpu")  # Applicable to Windows OS only
-#     options.add_argument("--no-sandbox")  # Bypass OS security model
-#     options.add_argument(
-#         "--disable-dev-shm-usage"
-#     )  # Overcome limited resource problems
-#     options.add_argument("--log-level=3")  # Suppress logs
-#     options.add_argument("--silent")  # Additional option to suppress logs
+def get_latest_chapter_from_page_element_sublist(
+    soup_html_parser: BeautifulSoup,
+) -> int | None:
+    """
+    Extracts the latest chapter number from a BeautifulSoup-parsed HTML page.
+    Searches for all chapter elements in paginated page sublist and extracts
+    the chapter number from the "a" element's href attribute of last chapter element
+    Args:
+        soup_html_parser (BeautifulSoup): A BeautifulSoup object containing the parsed HTML of the novel page.
+    Returns:
+        int | None: The latest chapter number if found, otherwise None.
+    """
+    chapter_element_list = soup_html_parser.find_all("div", class_="p-eplist__sublist")
 
-#     # Suppress DevTools logs
-#     options.set_capability(
-#         "goog:loggingPrefs", {"performance": "OFF", "browser": "OFF"}
-#     )
+    # chapter_title = chapter_element_list[-1].a.text.strip() if chapter_element_list
+    latest_chapter = (
+        int(chapter_element_list[-1].a["href"].split("/")[2])
+        if chapter_element_list
+        else None
+    )
+    return latest_chapter
 
-#     driver = webdriver.Chrome(
-#         service=ChromeService(ChromeDriverManager().install()), options=options
-#     )
-#     try:
-#         # Navigate to the URL
-#         driver.get(url)
 
-#         # Handle the age verification page
-#         if "novel18.syosetu.com" in url:
-#             time.sleep(2)  # Wait for the page to load
-#             enter_button = driver.find_element(By.ID, "yes18")
-#             enter_button.click()
+def get_latest_chapter_number_from_soup_parser(
+    soup_parser: BeautifulSoup, url: str, driver=None
+) -> int:
+    """
+    Extracts the latest chapter number from a given BeautifulSoup parser object.
+    Attempts to find the latest chapter from last pagination page and extracts the latest chapter number
+    If a web driver is provided, it will use the driver to navigate to the last page and handle age verification
+    otherwise, it will make an HTTP request to fetch the last page.
+    Args:
+        soup_parser (BeautifulSoup): The BeautifulSoup object containing the parsed HTML of the current page.
+        url (str): The base URL of the website being scraped.
+        driver (webdriver, optional): A Selenium WebDriver object for navigating pages. Defaults to None.
+    Returns:
+        int: The latest chapter number extracted from the page.
+    """
+    last_pagination = soup_parser.select_one("a.c-pager__item--last")
+    last_pagination_href = last_pagination.get("href") if last_pagination else None
 
-#         # Wait for the redirect to complete
-#         # driver.implicitly_wait(2)
+    if last_pagination_href:
+        paginate_last_url = urljoin(url, last_pagination_href)
+        if driver:
+            driver.get(paginate_last_url)
+            soup_last_parser = BeautifulSoup(driver.page_source, "html.parser")
+        else:
+            response_last_page = requests.get(paginate_last_url, headers=HEADERS)
+            soup_last_parser = BeautifulSoup(response_last_page.content, "html.parser")
 
-#         # Get the page source and parse it with BeautifulSoup
-#         soup = BeautifulSoup(driver.page_source, "html.parser")
-#         # Find the container with class "novelview_pager-last" to paginate to the latest chapter
-#         last_pager = soup.find("a", {"class": "c-pager__item c-pager__item--last"})
-#         logging.info(f"last_pager: {last_pager}")
-
-#         last_pager_href = None
-#         if last_pager:
-#             last_pager_href = last_pager.get("href")
-
-#         # Find container with class "index_box" if without paginate div
-#         index_box = soup.find("div", class_="index_box")
-#         # logging.info(f"last_pager_href: {last_pager_href}")
-
-#         if last_pager_href:
-#             driver.get(f"https://ncode.syosetu.com/{last_pager_href}")
-#             # driver.implicitly_wait(2)  # Wait for the page to load
-#             soup_last_page = BeautifulSoup(driver.page_source, "html.parser")
-
-#             novel_view_stats = soup_last_page.find(
-#                 "div", class_="c-pager__result-stats"
-#             ).get_text()
-#             stats_split = str.replace(novel_view_stats, "\xa0", " ").split(" ")
-#             return int(stats_split[3])
-#         # If there is no pagination, check the index box
-#         elif index_box:
-#             # Find all dl elements with class "novel_sublist2" inside the "index_box"
-#             chapter_list = index_box.find_all("dl", class_="novel_sublist2")
-
-#             if chapter_list:
-#                 # Find the last "dl" element in the list
-#                 last_chapter = chapter_list[-1]
-#                 # Extract the chapter title
-#                 chapter_title = last_chapter.find(
-#                     "dd", class_="subtitle"
-#                 ).a.text.strip()
-#                 # Extract the chapter number from the "a" element's href attribute
-#                 latest_chapter = int(
-#                     last_chapter.find("dd", class_="subtitle").a["href"].split("/")[-2]
-#                 )
-#                 # Print the last chapter number and title
-#                 # print(f"Last Chapter Number: {latest_chapter}")
-#                 # print(f"Last Chapter Title: {chapter_title}")
-
-#                 return latest_chapter
-#             else:
-#                 print("No chapters found in the list")
-#     finally:
-#         driver.quit()
+        return get_latest_chapter_from_page_element_sublist(soup_last_parser)
+    else:
+        return get_latest_chapter_from_page_element_sublist(soup_parser)
 
 
 def get_novel_latest_chapter_ncode(url: str) -> int:
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110"
-    }
-    # Send an HTTP GET request to the URL with header
-    response = requests.get(url, headers=headers)
-    # Parse the HTML content of the page
-    soup = BeautifulSoup(response.content, "html.parser")
+    """
+    Fetches the latest chapter number of a novel from the given URL with ncode.
+    Args:
+        url (str): The URL of the novel's webpage.
+    Returns:
+        int: The latest chapter number of the novel.
+    Raises:
+        requests.exceptions.RequestException: If there is an issue with the network request.
+        ValueError: If the chapter number cannot be extracted from the response.
+    """
+    response = requests.get(url, headers=HEADERS)
+    soup_parser = BeautifulSoup(response.content, "html.parser")
+    return get_latest_chapter_number_from_soup_parser(soup_parser, url)
 
-    # Find if page paginater exists
-    last_pager_href = None
-    last_pager = soup.select_one("a.c-pager__item--last")
 
-    if last_pager:
-        last_pager_href = last_pager.get("href")
-    # logging.info(f"last_pager_href: {last_pager_href}")
+def get_novel_latest_chapter_nocturne(url: str) -> int:
+    """
+    Retrieves the latest chapter number of a nocturne novel from the given URL.
+    Args:
+        url (str): The URL of the novel's page on the Nocturne website.
+    Returns:
+        int: The latest chapter number of the novel.
+    Raises:
+        NoSuchElementException: If the "yes18" button is not found on the page.
+        WebDriverException: If there is an issue with the WebDriver.
+    """
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.implicitly_wait(2)
 
-    # If paginater found, get the last page number and extract the last chapter number
-    if last_pager_href:
-        # https://ncode.syosetu.com/n4913gc/
-        # https://novel18.syosetu.com/n4913gc/
+    try:
+        driver.get(url)
+        enter_button = driver.find_element(By.ID, "yes18")
+        enter_button.click()
 
-        # base_url = f"https://ncode.syosetu.com{last_pager_href}"
-        base_url = urljoin(url, last_pager_href)
-        response_last_page = requests.get(base_url, headers=headers)
-        soup_last_page = BeautifulSoup(response_last_page.content, "html.parser")
-
-        novel_view_stats = soup_last_page.find(
-            "div", class_="c-pager__result-stats"
-        ).get_text()
-        # logging.debug(f"table_operations - novel_view_stats - {novel_view_stats}")
-        stats_split = str.replace(novel_view_stats, "\xa0", " ").split(" ")
-        # logging.debug(f"table_operations - stats_split - {stats_split}")
-        return int(stats_split[3])
-
-    # If there is no pagination, chapter element list
-    else:
-        # Find all chapter elements on main novel page
-        chapter_element_list = soup.find_all("div", class_="p-eplist__sublist")
-
-        if chapter_element_list:
-            # Extract the chapter title
-            chapter_title = chapter_element_list[-1].a.text.strip()
-            # Extract the chapter number from the "a" element's href attribute
-            latest_chapter = int(chapter_element_list[-1].a["href"].split("/")[2])
-
-            return latest_chapter
-        else:
-            print("No chapters found in the list")
+        soup_parser = BeautifulSoup(driver.page_source, "html.parser")
+        return get_latest_chapter_number_from_soup_parser(soup_parser, url, driver)
+    finally:
+        driver.quit()
